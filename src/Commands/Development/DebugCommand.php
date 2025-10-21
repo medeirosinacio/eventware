38d576647fabc46310b49c337ae9a688f13b83c3
Douglas Medeiros <douglas.medeiros@picpay.com>
2025-10-21 18:15:11 -0300

    initialize laravel project structure

--- src/Commands/Development/DebugCommand.php
+++ src/Commands/Development/DebugCommand.php
@@ -8,7 +8,7 @@ final class DebugCommand
 {
     public static function run(): true
     {
-        $event = (new \Eluceo\iCal\Domain\Entity\Event())
+        $event = (new \Eluceo\iCal\Domain\Entity\Event)
             ->setSummary('Christmas Eve')
             ->setDescription('Lorem Ipsum Dolor... Test Sync')
             ->setLocation(
@@ -17,12 +17,12 @@ final class DebugCommand
             ->setOccurrence(
                 new \Eluceo\iCal\Domain\ValueObject\SingleDay(
                     new \Eluceo\iCal\Domain\ValueObject\Date(
-                        \DateTimeImmutable::createFromFormat('Y-m-d', '2030-12-24')
+                        \DateTimeImmutable::createFromFormat('Y-m-d', '2030-12-24') ?: new \DateTimeImmutable
                     )
                 )
             );
         $calendar = new \Eluceo\iCal\Domain\Entity\Calendar([$event]);
-        $iCalendarComponent = (new \Eluceo\iCal\Presentation\Factory\CalendarFactory())->createCalendar($calendar);
+        $iCalendarComponent = (new \Eluceo\iCal\Presentation\Factory\CalendarFactory)->createCalendar($calendar);
 
         file_put_contents('/app/runtime/tmp/calendar.ics', (string) $iCalendarComponent);
 
--- src/Enums/ExternalCalendars.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Eventware\Enums;
-
-enum ExternalCalendars
-{
-    case FIFA_CLUB_WORLD_CUP;
-}
